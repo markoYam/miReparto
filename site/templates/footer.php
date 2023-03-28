a1f9a195fb05cd74498c6c915a249ff70bc164dc
Diego Arcos <berna1996arcos@gmail.com>
2023-03-28 13:00:31 -0600

    se envia cambios de agregar un dashboard

---
+++ site/templates/footer.php
@@ -0,0 +1,15 @@
+                <footer class="py-4 bg-light mt-auto">
+                    <div class="container-fluid px-4">
+                        <div class="d-flex align-items-center justify-content-between small">
+                            <div class="text-muted">Copyright &copy; Aurelio Reparte 2023</div>
+                            
+                        </div>
+                    </div>
+                </footer>
+            </div>
+        </div>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="<?=URL_BASE?>site/assets/js/scripts.js"></script>
+        
+    </body>
+</html>
---
+++ site/templates/sidebar.php
@@ -0,0 +1,24 @@
+<div id="layoutSidenav_nav">
+    <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
+        <div class="sb-sidenav-menu">
+            <div class="nav">
+                <div class="sb-sidenav-menu-heading">Menú</div>
+                <a class="nav-link" href="<?=URL_BASE?>site/templates/paradas">
+                    <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
+                    Paradas
+                </a>
+                <a class="nav-link" href="<?=URL_BASE?>site/templates/Rutas">
+                    <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
+                    Rutas
+                </a>
+                
+                
+                
+            </div>
+        </div>
+        <div class="sb-sidenav-footer">
+            <div class="small">Logueado:</div>
+            Aurelio Reparte
+        </div>
+    </nav>
+</div>
